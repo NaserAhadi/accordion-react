e4c8dd345edd42c2591a460989df59720fc2122b
Naser Ahadi <n.ahadi@vandar.io>
2025-01-10 18:56:15 +0330

    handle opening only one Accordion | handle toggling self Accordion

--- src/App.js
+++ src/App.js
@@ -1,47 +1,58 @@
 import "./App.css";
 import { useState } from "react";
 
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa cumque ea fugit labore nobis veritatis. Ad aliquam beatae corporis doloremque, dolorum ducimus expedita harum, ipsa natus quam saepe sit suscipit.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa cumque ea fugit labore nobis veritatis. Ad aliquam beatae corporis doloremque, dolorum ducimus expedita harum, ipsa natus quam saepe sit suscipit.",
+  },
+  {
+    title: "Do you ship to countries outside EU?",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa cumque ea fugit labore nobis veritatis. Ad aliquam beatae corporis doloremque, dolorum ducimus expedita harum, ipsa natus quam saepe sit suscipit.",
+  },
+];
+
 export default function App() {
   return (
     <div className="App">
-      <Accordion />
+      <Accordion data={faqs} />
     </div>
   );
 }
 
-function Accordion() {
-  const faqs = [
-    {
-      title: "Where are these chairs assembled?",
-      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa cumque ea fugit labore nobis veritatis. Ad aliquam beatae corporis doloremque, dolorum ducimus expedita harum, ipsa natus quam saepe sit suscipit.",
-    },
-    {
-      title: "How long do I have to return my chair?",
-      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa cumque ea fugit labore nobis veritatis. Ad aliquam beatae corporis doloremque, dolorum ducimus expedita harum, ipsa natus quam saepe sit suscipit.",
-    },
-    {
-      title: "Do you ship to countries outside EU?",
-      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa cumque ea fugit labore nobis veritatis. Ad aliquam beatae corporis doloremque, dolorum ducimus expedita harum, ipsa natus quam saepe sit suscipit.",
-    },
-  ];
+function Accordion({ data }) {
+  const [selectedId, setSelectedId] = useState(null);
+
+  function handleToggle(id) {
+    setSelectedId(id);
+  }
   return (
     <div className="accordions-container">
-      {faqs.map((faq, index) => (
-        <AccordionElement faq={faq} key={index} index={index} />
+      {data.map((faq, index) => (
+        <div key={index}>
+          <AccordionElement
+            faq={faq}
+            index={index}
+            isOpen={index === selectedId}
+            onSetSelected={handleToggle}
+          />
+        </div>
       ))}
     </div>
   );
 }
 
-function AccordionElement({ faq, index }) {
-  const [isOpen, setIsOpen] = useState(false);
-
+function AccordionElement({ faq, index, isOpen, onSetSelected }) {
   return (
-    <div className={isOpen ? "accordion-card is-open-card" : "accordion-card"}>
-      <button
-        className="collapsible"
-        onClick={() => setIsOpen((isOpen) => !isOpen)}
-      >
+    <div
+      onClick={() => onSetSelected(isOpen ? null : index)}
+      className={isOpen ? "accordion-card is-open-card" : "accordion-card"}
+    >
+      <button className="collapsible">
         <h2
           className={
             isOpen
@@ -51,8 +62,7 @@ function AccordionElement({ faq, index }) {
         >
           {`0${index + 1}`}
         </h2>
-        <h2 className={isOpen && "is-open-card-title"}>{faq.title}</h2>
-
+        <h2 className={isOpen ? "is-open-card-title" : ""}>{faq.title}</h2>
         <h2 className="collapsible-icon">{isOpen ? "-" : "+"}</h2>
       </button>
       <p className={isOpen ? "content is-open-card-content" : "content"}>
